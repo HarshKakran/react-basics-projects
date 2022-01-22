dd7e2c459acf68256ef3c748e83337ab9046d814
HarshKakran <harshkakran05@gamil.com>
2022-01-22 19:05:16 +0530

    Completed Menu Items project

--- useEffect and conditional rendering/5-menu-items/src/Categories.js
+++ useEffect and conditional rendering/5-menu-items/src/Categories.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ items }) => {
+  console.log(items, typeof items);
+  return (
+    <>
+      <div className="section-center">
+        {items.map((item) => {
+          const { id, title, category, price, img, desc } = item;
+          return (
+            <article className="menu-item" key={id}>
+              <img src={img} alt={title} />
+              <div className="item-info">
+                <header>
+                  <h4>{title}</h4>
+                  <h4 className="price">{price}</h4>
+                </header>
+                <p className="item-text">{desc}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Categories;
--- useEffect and conditional rendering/5-menu-items/src/Menu.js
+++ useEffect and conditional rendering/5-menu-items/src/Menu.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Categories from "./Categories";
+import data from "./data";
 
 const Menu = () => {
-  return <h2>menu component</h2>;
+  const [category, setCategory] = useState("all");
+  const [items, setItems] = useState(data);
+  //   const [isBreakfast, setIsBreakfast] = useState(false);
+  //   const [isLunch, setIsLunch] = useState(false);
+  //   const [isShakes, setIsShakes] = useState(false);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setItems(data);
+    } else {
+      const newData = data.filter((item) => item.category === category);
+      setItems(newData);
+    }
+  };
+  return (
+    <>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="btn-container">
+          <button className="filter-btn" onClick={() => filterItems("all")}>
+            all
+          </button>
+          <button
+            className="filter-btn"
+            onClick={() => filterItems("breakfast")}
+          >
+            breakfast
+          </button>
+          <button className="filter-btn" onClick={() => filterItems("lunch")}>
+            lunch
+          </button>
+          <button className="filter-btn" onClick={() => filterItems("shakes")}>
+            shakes
+          </button>
+        </div>
+        <Categories items={items}></Categories>
+      </section>
+    </>
+  );
 };
 
 export default Menu;
